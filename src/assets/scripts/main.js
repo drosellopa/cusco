62e30d8643cc61f1297cda741b9f6e657c8de807
Daniel Roselló Parra <drosellopa@uoc.edu>
2025-10-25 23:39:35 +0200

    First commit HTML and CSS mobile version created. Only header and menu is-open created on JS

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,51 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
+
+//Main header
+
+//DOM
+const mainHeader = document.getElementById('main-header');
+const barsMenu = document.querySelector('.fa-bars');
+const mainMenu = document.querySelector('.main-menu');
+const mainMenuItem = document.querySelectorAll('.main-menu a');
+
+//Variables
+let mainMenuHidden = true;
+
+//EVENTS
+//WINDOW SCROLL
+window.addEventListener('scroll', e=>{
+  if (window.scrollY > 100){
+    mainHeader.classList.add('main-header-fixed');
+  } else {
+    mainHeader.classList.remove('main-header-fixed');
+  }  
+});
+
+//Bars menu click
+barsMenu.addEventListener('click', ()=> toggleMenu());
+//Item menu click
+mainMenuItem.forEach((e)=>{
+  e.addEventListener('click', ()=> toggleMenu());
+});
+
+
+function toggleMenu(){
+  
+  mainMenu.classList.toggle('is-open');
+}
+
